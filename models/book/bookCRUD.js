b3f821ead3c4b5c7f9b6be225ea9b7c6c6b9cfe8
ariefkahfi <ariefkahfi@gmail.com>
2018-01-14 10:26:57 +0700

    add function to BookCRUD

--- models/book/bookCRUD.js
+++ models/book/bookCRUD.js
@@ -2,7 +2,28 @@ var databaseModel = require("../databasemodel");
 
 
 function BookCRUD(){
+    this.save = function(obj){
+        databaseModel.bookModel({
+            name : obj.book.name,
+            price : obj.book.price
+        }).save().then(function(){
+            obj.onFinish();
+        }).catch(function(err){
+            obj.onError(err);
+        });
+    }
 
+    this.update = function(obj){
+        databaseModel.bookModel({
+            name : obj.book.name,
+            price : obj.book.price,
+            id : obj.book.id
+        }).update().then(function(){
+            obj.onFinish();
+        }).catch(function(err){
+            obj.onError(err);
+        });
+    }
 }
 
 module.exports = new BookCRUD();
