150320fdc60597823d092464a2d9160e956a5d87
ariefkahfi <ariefkahfi@gmail.com>
2018-01-14 10:36:56 +0700

    add function to cardCRUD

--- models/card/cardCRUD.js
+++ models/card/cardCRUD.js
@@ -2,7 +2,50 @@ var databaseModel = require("../databasemodel");
 
 
 function CardCRUD(){
-
+    this.save = function(obj){
+        databaseModel.cardModel({
+            id : obj.card.id,
+            type : obj.card.type,
+            member_id : obj.card.member_id
+        }).save()
+        .then(function(){
+            obj.onFinish();
+        }).catch(function(err){
+            obj.onError(err);
+        });
+    }
+    this.getCards = function(obj){
+        databaseModel
+            .cardModel()
+            .query("select * from card")
+            .then(function(results){
+                obj.onResult(results);
+            })
+            .catch(function(err){
+                obj.onError(err);
+            });
+    }
+    this.update = function(obj){
+        databaseModel.cardModel({
+            type : obj.card.type,
+            id : obj.card.id
+        }).update().then(function(){
+            obj.onFinish();
+        }).catch(function(err){
+            obj.onError(err);
+        });
+    }
+    this.delete = function(obj){
+        databaseModel
+            .dbInstance()
+            .statement("delete from card where id = @id",{id : obj.card.id})
+            .then(function(){
+                obj.onFinish();
+            })
+            .catch(function(err){
+                obj.onError(err);
+            });
+    }
 }
 
 module.exports = new CardCRUD();
